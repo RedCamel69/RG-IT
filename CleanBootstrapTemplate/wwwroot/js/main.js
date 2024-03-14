34db96769e35c5b7a8a49c81bc06c41f18837411
Richard Gilbert <RichardJGilbert@hotmail.com>
2024-03-14 15:44:47 +0000

    Initial commit

---
+++ CleanBootstrapTemplate/wwwroot/js/main.js
@@ -0,0 +1,22 @@
+﻿export function ScrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+export function onScroll(interval) {
+    let selectHeader = document.getElementById('header');
+    /*let selectBackToTop = document.getElementById('back-to-top');*/
+    window.addEventListener('scroll', function (e) {       
+       
+        if (selectHeader) {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled');
+               /* selectBackToTop.classList.add('active');*/
+            } else {
+                selectHeader.classList.remove('header-scrolled');
+              /*  selectBackToTop.classList.remove('active');*/
+            }
+        }
+    }, interval);
+}
+
